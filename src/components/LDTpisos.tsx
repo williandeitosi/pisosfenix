7dedd1ac587cfa9289865ece068c1aafccc0f29b
williandeitosi <williandeitosi@gmail.com>
2025-01-30 00:36:19 -0300

    fix: change title osso cliente to Alguns de nossos clientes

--- src/components/LDTpisos.tsx
+++ src/components/LDTpisos.tsx
@@ -231,7 +231,7 @@ const StructureSection: React.FC = () => {
         </div>
         <div className="flex flex-col items-center justify-center px-4 md:px-8">
           <h3 className="text-2xl md:text-3xl lg:text-4xl font-bold text-white text-center my-8 md:my-12">
-            Nossos Clientes
+            Alguns de nossos clientes
           </h3>
 
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-7 gap-6 md:gap-8 max-w-7xl">
